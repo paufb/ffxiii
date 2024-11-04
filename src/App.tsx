6e43152ca3c9e151edd231adf016883a6a3675a0
Pau Fernandez Bardulet <58038009+paufb@users.noreply.github.com>
2024-11-04 20:20:59 +0100

    Add home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { useRoutes } from 'react-router-dom';
 import './App.css'
 import './fonts.css';
+import { MainMenuPage } from './pages/MainMenu.tsx';
 import { InventoryPage } from './pages/InventoryPage.tsx';
 
 function App() {
   const routes = useRoutes([
-    { path: '/', element: <></> },
+    { path: '/', element: <MainMenuPage /> },
     { path: '/inventory', element: <InventoryPage /> }
   ]);
 
