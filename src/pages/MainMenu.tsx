078e4dcc7c3bc08b4124bd8a0d888a9cbded8b8c
Pau Fernandez Bardulet <58038009+PauFB@users.noreply.github.com>
2024-11-16 15:10:43 +0100

    Add character cards info.

--- src/pages/MainMenu.tsx
+++ src/pages/MainMenu.tsx
@@ -1,6 +1,7 @@
 import { NavLink } from 'react-router-dom';
-import { Character, MainMenuCategory } from '../types.ts';
+import { Character, MainMenuCategory, Role } from '../types.ts';
 import CursorIcon from '../assets/FFXIII_Pointer_White_BG.webp';
+import PartyLeaderIcon from '../assets/Leader-ffxiii-icon.png';
 import styles from './MainMenu.module.css';
 import pageAnimations from './page-animations.module.css';
 
@@ -9,14 +10,47 @@ export function MainMenuPage() {
     'Inventory': '/inventory',
     'Datalog': '/datalog'
   };
-  const charactersInfo: { [key in Character]: { role?: string, hp: number, cp: number, imgURL: string } } = {
-    'Lightning': { role: 'Commando', hp: 3000, cp: 0, imgURL: new URL('../assets/Lightning.png', import.meta.url).href },
-    'Vanille': { role: 'Medic', hp: 3000, cp: 0, imgURL: new URL('../assets/Vanille.png', import.meta.url).href },
-    'Fang': { role: 'Ravager', hp: 3000, cp: 0, imgURL: new URL('../assets/Fang.png', import.meta.url).href },
-    'Snow': { hp: 3000, cp: 0, imgURL: new URL('../assets/Snow.png', import.meta.url).href },
-    'Sazh': { hp: 3000, cp: 0, imgURL: new URL('../assets/Sazh.png', import.meta.url).href },
-    'Hope': { hp: 3000, cp: 0, imgURL: new URL('../assets/Hope.png', import.meta.url).href }
+  const charactersInfo: { [key in Character]: { role?: Role, imgURL: string } } = {
+    'Lightning': { role: 'Commando', imgURL: new URL('../assets/Lightning.png', import.meta.url).href },
+    'Vanille': { role: 'Medic', imgURL: new URL('../assets/Vanille.png', import.meta.url).href },
+    'Fang': { role: 'Ravager', imgURL: new URL('../assets/Fang.png', import.meta.url).href },
+    'Snow': { imgURL: new URL('../assets/Snow.png', import.meta.url).href },
+    'Sazh': { imgURL: new URL('../assets/Sazh.png', import.meta.url).href },
+    'Hope': { imgURL: new URL('../assets/Hope.png', import.meta.url).href }
   };
+
+  function renderRole(role: Role | undefined) {
+    if (!role) return;
+    const roleColors: Record<Role, string> = {
+      'Commando': 'red',
+      'Ravager': 'rgb(133, 145, 194)',
+      'Sentinel': 'yellow',
+      'Saboteur': 'gray',
+      'Synergist': 'purple',
+      'Medic': 'rgb(204, 255, 204)'
+    };
+    return (
+      <div
+        className={`${styles['character-card-role']} ${styles['text-outlined']}`}
+        style={{
+          background: `-webkit-linear-gradient(${roleColors[role]} -80%, white 100%)`,
+          WebkitBackgroundClip: 'text',
+          WebkitTextFillColor: 'transparent'
+        }}
+      >
+        {role?.slice(0, 3)}
+      </div>
+    );
+  }
+
+  function renderPartyStatus(index: number) {
+    return index >= 0 && index <= 2 && (
+      <div className={`${styles['character-card-party-status']} ${styles['text-outlined']}`}>
+        {index === 0 ? 'Leader' : index === 1 || index === 2 ? 'Member' : null}
+        <img src={PartyLeaderIcon} style={{ filter: `grayscale(${index === 0 ? '0' : '1'})` }} />
+      </div>
+    );
+  }
 
   return (
     <div className={styles['page']}>
@@ -30,7 +64,7 @@ export function MainMenuPage() {
             <li key={categoryName} className={styles['category']}>
               <img className={styles['category-cursor']} src={CursorIcon} alt="" />
               <NavLink to={route}>
-                {categoryName}
+                <span>{categoryName}</span>
               </NavLink>
             </li>
           ))}
@@ -38,12 +72,19 @@ export function MainMenuPage() {
       </div>
       {Object.entries(charactersInfo).map(([characterName, character], index) => (
         <div
-          className={`${styles['character-card']} ${styles[`character-card--${characterName.toLowerCase()}`]} ${pageAnimations['fade-in-from-top']}`}
+          key={characterName}
+          className={`${styles['character-card']} ${styles[`character-card--${characterName.toLowerCase()}`]} ${!character?.role ? styles['character-card-grayscale'] : ''} ${pageAnimations['fade-in-from-top']}`}
           style={{ animationFillMode: 'backwards', animationDelay: `${index / 10}s` }}
         >
           <img src={character.imgURL} />
-          {/* {character.hp}
-          {character.cp} */}
+          {renderRole(character.role)}
+          {renderPartyStatus(index)}
+          <div className={`${styles['character-card-stats']} ${styles['text-outlined']}`}>
+            <div className={styles['character-card-hp-label']}>HP</div>
+            <div className={styles['character-card-value']}>{Math.round(3000 * (1 + (Math.random() * 0.6 - 0.3)))}</div>
+            <div className={styles['character-card-cp-label']}>CP</div>
+            <div className={styles['character-card-value']}>0</div>
+          </div>
         </div>
       ))}
     </div>
--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,5 @@
 export type Character = 'Lightning' | 'Vanille' | 'Fang' | 'Snow' | 'Sazh' | 'Hope';
+export type Role = 'Commando' | 'Ravager' | 'Sentinel' | 'Saboteur' | 'Synergist' | 'Medic';
 
 export type MainMenuCategory = 'Inventory' | 'Datalog';
 export type InventoryCategory = 'Items' | 'Weapons' | 'Accessories' | 'Components' | 'Key Items';
