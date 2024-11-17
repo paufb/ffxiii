46b342ec3e08dfb06b4c290d397e8746de198d56
Pau Fernandez Bardulet <58038009+PauFB@users.noreply.github.com>
2024-11-17 11:54:42 +0100

    Animate help header

--- src/components/HelpHeader.tsx
+++ src/components/HelpHeader.tsx
@@ -1,4 +1,5 @@
 import styles from './HelpHeader.module.css';
+import pageAnimations from '../pages/page-animations.module.css';
 
 interface HelpHeaderProps {
   theme: 'light' | 'dark';
@@ -8,7 +9,7 @@ export function HelpHeader({ theme }: HelpHeaderProps) {
   const outlinedClass = theme === 'light' ? 'outlined' : '';
 
   return (
-    <div className={`${styles['header']} ${styles[theme]}`}>
+    <div className={`${styles['header']} ${styles[theme]} ${pageAnimations['fade-in-from-top']}`}>
       <div className={`${styles['ruler']} ${outlinedClass}`} />
       <div className={styles['help-container']}>
         <span className={`${styles['int']} ${outlinedClass}`}>
