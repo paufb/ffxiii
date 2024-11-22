da72890cdd5ce1176428692064cd258b7a62912d
Pau Fernandez Bardulet <58038009+PauFB@users.noreply.github.com>
2024-11-23 00:54:10 +0100

    Add highlightable party characters to inventory page

--- src/pages/InventoryPage.tsx
+++ src/pages/InventoryPage.tsx
@@ -1,8 +1,9 @@
 import { useState } from 'react';
-import { InventoryCategory } from '../types.ts';
+import { Character, InventoryCategory } from '../types.ts';
 import { AccessoriesInventory } from '../components/AccessoriesInventory.tsx';
 import { ComponentsInventory } from '../components/ComponentsInventory.tsx';
 import { Cursor } from '../components/Cursor.tsx';
+import { InventoryParty } from '../components/InventoryParty.tsx';
 import { ItemsInventory } from '../components/ItemsInventory.tsx';
 import { KeyItemsInventory } from '../components/KeyItemsInventory.tsx';
 import { WeaponsInventory } from '../components/WeaponsInventory.tsx';
@@ -15,6 +16,7 @@ interface InventoryPageProps {
 
 export function InventoryPage({ setHelpHeaderText }: InventoryPageProps) {
   const [activeCategory, setActiveCategory] = useState<InventoryCategory>('Items');
+  const [highlightedCharacter, setHighlightedCharacter] = useState<Character>();
   const categoriesMap: { [key in InventoryCategory]: { headerText: string } } = {
     'Items': { headerText: 'View consumable items in the party inventory.' },
     'Weapons': { headerText: 'View weapons in the party inventory.' },
@@ -26,12 +28,17 @@ export function InventoryPage({ setHelpHeaderText }: InventoryPageProps) {
   function renderInventoryCategory(category: InventoryCategory) {
     const categoriesMap: { [key in InventoryCategory]: JSX.Element } = {
       'Items': <ItemsInventory setHelpHeaderText={setHelpHeaderText} />,
-      'Weapons': <WeaponsInventory setHelpHeaderText={setHelpHeaderText} />,
+      'Weapons': <WeaponsInventory setHighlightedCharacter={setHighlightedCharacter} setHelpHeaderText={setHelpHeaderText} />,
       'Accessories': <AccessoriesInventory />,
       'Components': <ComponentsInventory />,
       'Key Items': <KeyItemsInventory />
     };
     return categoriesMap[category];
+  }
+
+  function handleOnClickCategory(category: InventoryCategory) {
+    setActiveCategory(category);
+    setHighlightedCharacter(undefined);
   }
 
   return (
@@ -46,7 +53,7 @@ export function InventoryPage({ setHelpHeaderText }: InventoryPageProps) {
             <li
               key={category}
               className={`${styles['category']} ${activeCategory === category ? styles['active'] : ''}`}
-              onClick={() => setActiveCategory(category as InventoryCategory)}
+              onClick={() => handleOnClickCategory(category as InventoryCategory)}
               onMouseEnter={() => setHelpHeaderText(headerText)}
               onMouseLeave={() => setHelpHeaderText(categoriesMap[activeCategory].headerText)}
             >
@@ -56,7 +63,8 @@ export function InventoryPage({ setHelpHeaderText }: InventoryPageProps) {
           ))}
         </ul>
       </div>
-      <div className={pageAnimations['fade-in-from-right']}>
+      <div className={`${styles['inventory-column']} ${pageAnimations['fade-in-from-right']}`}>
+        <InventoryParty highlightedCharacter={highlightedCharacter} />
         {renderInventoryCategory(activeCategory)}
       </div>
     </div>
