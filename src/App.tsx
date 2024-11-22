8c975c2cd4e957b054c13c8e434a36f03f0a8cfe
Pau Fernandez Bardulet <58038009+PauFB@users.noreply.github.com>
2024-11-22 21:53:35 +0100

    Set help header text from MainMenuPage and InventoryPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
+import { useState } from 'react';
 import { useRoutes } from 'react-router-dom';
-import './App.css'
-import './fonts.css';
-import { MainMenuPage } from './pages/MainMenu.tsx';
+import { MemoizedMainMenuPage } from './pages/MainMenuPage.tsx';
 import { InventoryPage } from './pages/InventoryPage.tsx';
 import { HelpHeader } from './components/HelpHeader.tsx';
+import './App.css'
+import './fonts.css';
 
 function App() {
+  const [text, setText] = useState('');
   const routes = useRoutes([
-    { path: '/', element: <MainMenuPage /> },
-    { path: '/inventory', element: <InventoryPage /> }
+    { path: '/', element: <MemoizedMainMenuPage setHelpHeaderText={setText} /> },
+    { path: '/inventory', element: <InventoryPage setHelpHeaderText={setText} /> }
   ]);
 
   return (
     <>
       <div className="background" />
-      <HelpHeader theme={'dark'} />
+      <HelpHeader theme={'dark'} text={text} />
       {routes}
     </>
   );
