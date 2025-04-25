f1f5e19512693bb4e1b31d7b9544856e3e5b11ea
Pau Fernandez Bardulet <58038009+paufb@users.noreply.github.com>
2025-04-26 01:17:40 +0200

    Initialize Angular project

--- src/components/StarIcon.tsx
+++
@@ -1,7 +0,0 @@
-export function StarIcon({ color = 'black', height = '1rem', width = '1rem' }) {
-  return (
-    <svg fill={color} height={height} width={width} viewBox="0 0 512 512">
-      <polygon points="512,207.9 315.1,207.9 256,11 196.9,207.9 0,207.9 157.5,316.2 98.5,503.3 256,404.8 413.5,503.3 354.5,316.2 "/>
-    </svg>
-  );
-}
