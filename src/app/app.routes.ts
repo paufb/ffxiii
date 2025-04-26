86734fc18c3eae0aaed0a7f094f971b37ddf4907
Pau Fernandez Bardulet <58038009+paufb@users.noreply.github.com>
2025-04-26 16:32:45 +0200

    Add MainMenu page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { MainMenuComponent } from './pages/main-menu/main-menu.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: MainMenuComponent }
+];
---
+++ src/app/pages/main-menu/main-menu.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-menu',
+  imports: [],
+  templateUrl: './main-menu.component.html',
+  styleUrl: './main-menu.component.css'
+})
+export class MainMenuComponent {}
