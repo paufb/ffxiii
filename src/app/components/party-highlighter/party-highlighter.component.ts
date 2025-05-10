65a6d5dc953354759dd90afbcc6ebd2bd45622a5
Pau Fernandez Bardulet <58038009+paufb@users.noreply.github.com>
2025-05-11 00:33:04 +0200

    Update PartyHighlighterService service to use signals

--- src/app/components/party-highlighter/party-highlighter.component.ts
+++ src/app/components/party-highlighter/party-highlighter.component.ts
@@ -1,20 +1,17 @@
 import { NgOptimizedImage } from '@angular/common';
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { characters } from '../../constants/characters';
-import { Character } from '../../models/character.model';
 import { PartyHighlighterService } from '../../services/party-highlighter.service';
 
 @Component({
   selector: 'app-party-highlighter',
   imports: [NgOptimizedImage],
   templateUrl: './party-highlighter.component.html',
-  styleUrl: './party-highlighter.component.css'
+  styleUrl: './party-highlighter.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class PartyHighlighterComponent {
-  characters = Object.values(characters);
-  highlightedCharacter!: Character | null;
-  
-  constructor(private partyHighlighterService: PartyHighlighterService) {
-    this.partyHighlighterService.$highlightedCharacter.subscribe((character) => this.highlightedCharacter = character);
-  }
+  private readonly partyHighlighterService = inject(PartyHighlighterService);
+  protected readonly highlightedCharacter = this.partyHighlighterService.highlightedCharacter;
+  protected readonly characters = Object.values(characters);
 }
--- src/app/services/party-highlighter.service.ts
+++ src/app/services/party-highlighter.service.ts
@@ -1,15 +1,14 @@
-import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { Injectable, signal } from '@angular/core';
 import { Character } from '../models/character.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PartyHighlighterService {
-  highlightedCharacterSubject = new BehaviorSubject<Character | null>(null);
-  $highlightedCharacter = this.highlightedCharacterSubject.asObservable();
+  private readonly _highlightedCharacter = signal<Character | null>(null);
+  readonly highlightedCharacter = this._highlightedCharacter.asReadonly();
 
   setHighlightedCharacter(character: Character | null) {
-    this.highlightedCharacterSubject.next(character);
+    this._highlightedCharacter.set(character);
   }
 }
