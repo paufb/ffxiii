3be71dcf92aa7897e9dbeecb824eb6c98a020ee2
Pau Fernandez Bardulet <58038009+paufb@users.noreply.github.com>
2025-05-10 20:54:15 +0200

    Wrap AppComponent's RouterOutlet in grid and update route transition animations

--- src/app/route-animations.ts
+++ src/app/route-animations.ts
@@ -1,4 +1,4 @@
-import { animate, group, query, sequence, stagger, style, transition, trigger } from '@angular/animations';
+import { animate, group, query, stagger, style, transition, trigger } from '@angular/animations';
 
 function fade(
   origin: 'from' | 'to',
@@ -26,42 +26,55 @@ function fade(
 export const appRouteAnimationsTrigger = trigger('routeAnimations', [
   transition('MainMenuPage => InventoryPage', [
     group([
-      query(':leave', style({ position: 'fixed' })),
-      query(':leave app-navigation-menu', fade('to', 'left', '5rem')),
-      query(':leave .animatable', style({ width: '1032px' })),
-      query(':leave .animatable app-character-card', fade('to', 'top', '65rem', -25)),
-      query(':enter app-navigation-menu', fade('from', 'left', '5rem')),
-      query(':enter .animatable', sequence([
-        style({ position: 'fixed', width: '992px', height: 'min-content' }),
-        ...fade('from', 'right', '65rem')
-      ]))
-    ]),
+      query('app-main-menu', style({ overflow: 'visible' })),
+      query(':leave', [
+        group([
+          query('.animatable', style({ overflow: 'visible' })),
+          query('app-navigation-menu', fade('to', 'left', '5rem')),
+          query('.animatable app-character-card', fade('to', 'top', '65rem', -25))
+        ])
+      ]),
+      query('app-inventory', style({ overflow: 'visible' })),
+      query(':enter', [
+        group([
+          query('app-inventory > app-navigation-menu', fade('from', 'left', '5rem')),
+          query('app-inventory > div', fade('from', 'right', '65rem'))
+        ])
+      ])
+    ])
   ]),
   transition('InventoryPage => MainMenuPage', [
     group([
-      query(':leave .animatable', sequence([
-        style({ position: 'fixed', width: '992px' }),
-        ...fade('to', 'right', '65rem')
-      ])),
-      query(':leave', style({ position: 'fixed' })),
-      query(':leave app-navigation-menu', fade('to', 'left', '5rem')),
-      query(':enter app-character-card', fade('from', 'top', '65rem', 25)),
-      query(':enter app-navigation-menu', fade('from', 'left', '5rem')),
+      query('app-inventory', style({ overflow: 'visible' })),
+      query(':leave', [
+        group([
+          query('app-inventory > app-navigation-menu', fade('to', 'left', '5rem')),
+          query('app-inventory > div', fade('to', 'right', '65rem'))
+        ])
+      ]),
+      query('app-main-menu', style({ overflow: 'visible' })),
+      query(':enter', [
+        group([
+          query('.animatable', style({ overflow: 'visible' })),
+          query('app-character-card', fade('from', 'top', '65rem', 25)),
+          query('app-navigation-menu', fade('from', 'left', '5rem'))
+        ])
+      ])
     ])
   ]),
   transition('* => MainMenuPage', [
     group([
+      query('app-main-menu', style({ overflow: 'visible' })),
+      query('.animatable', style({ overflow: 'visible' })),
       query(':enter app-navigation-menu', fade('from', 'left', '5rem')),
-      query(':enter app-character-card', fade('from', 'top', '10rem', 50)),
-    ]),
+      query(':enter app-character-card', fade('from', 'top', '10rem', 50))
+    ])
   ]),
   transition('* => InventoryPage', [
     group([
-      query(':enter app-navigation-menu', fade('from', 'left', '5rem')),
-      query(':enter .animatable', sequence([
-          style({ position: 'fixed', width: '992px' }),
-          ...fade('from', 'right', '5rem')
-      ]))
-    ]),
+      query('app-inventory', style({ overflow: 'visible' })),
+      query('app-inventory > app-navigation-menu', fade('from', 'left', '5rem')),
+      query('app-inventory > div', fade('from', 'right', '5rem'))
+    ])
   ])
 ]);
