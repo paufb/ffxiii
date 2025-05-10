5d4e01a5b839f98788740c17527cf1bfdda3799d
Pau Fernandez Bardulet <58038009+paufb@users.noreply.github.com>
2025-05-11 01:43:17 +0200

    Remove CursorComponent's wrapper element

--- src/app/components/cursor/cursor.component.ts
+++ src/app/components/cursor/cursor.component.ts
@@ -1,13 +1,17 @@
 import { NgOptimizedImage } from '@angular/common';
-import { booleanAttribute, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-cursor',
   imports: [NgOptimizedImage],
   templateUrl: './cursor.component.html',
-  styleUrl: './cursor.component.css'
+  styleUrl: './cursor.component.css',
+  host: {
+    '[class.glide]': 'glide()'
+  },
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CursorComponent {
-  @Input({ transform: booleanAttribute }) selection: boolean = false;
-  @Input({ transform: booleanAttribute }) glide: boolean = false;
+  selection = input(false);
+  glide = input(false);
 }
